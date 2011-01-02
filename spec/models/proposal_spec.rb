5ce1f3a388d858d81954db8c42e828514d13aab6
Mario Aquino <mario.e.aquino@gmail.com>
2011-01-02 14:54:34 -0600

    Added simple model spec and shoulda dependency for Proposal model.

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe Proposal do
-  it "requires speakername" do
-    Proposal.new().valid?.should be_false
-  end
+  it { should be_invalid }
+   
+  it { should validate_presence_of :speaker_name }
+
+  it { should validate_presence_of :speaker_email }
+
+  it { should validate_presence_of :title }
+
+  it { should validate_presence_of :description }
 end
