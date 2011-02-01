da274e75166c67d00cac9ceb58ff57e0410fc590
Mario Aquino <mario.e.aquino@gmail.com>
2011-02-01 15:18:22 -0600

    Code cleanup

--- vendor/engines/proposals/spec/models/proposal_spec.rb
+++ vendor/engines/proposals/spec/models/proposal_spec.rb
@@ -2,33 +2,26 @@ require 'spec_helper'
 
 describe Proposal do
 
-  def reset_proposal(options = {})
-    @valid_attributes = {
-      :id => 1,
-      :status => 'submitted'
-    }
-  end
-
-  before(:each) do
-    reset_proposal
+  def valid_attributes(options = {})
+    { :id => 1, :status => 'submitted' }.merge(options)
   end
 
   context "validations" do
     
     it "rejects empty status" do
-      Proposal.new(@valid_attributes.merge(:status => "")).should_not be_valid
+      Proposal.new(valid_attributes(:status => "")).should_not be_valid
     end
 
     it "rejects invalid status" do
-      Proposal.new(@valid_attributes.merge(:status => 'heynow')).should_not be_valid
+      Proposal.new(valid_attributes(:status => 'heynow')).should_not be_valid
     end
     
     it "rejects when there is no Talk" do
-      Proposal.new(@valid_attributes).should_not be_valid      
+      Proposal.new(valid_attributes).should_not be_valid      
     end
 
     it "requires valid attributes and a related Talk" do
-      p = Proposal.new(@valid_attributes)
+      p = Proposal.new(valid_attributes)
       p.talk = Talk.new(:title => 'Writing a conf site', :abstract => 'Moar codez')
       p.should be_valid
     end
