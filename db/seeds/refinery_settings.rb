909fce13e5292e6ba0a7185409c4069dceab54d2
Mario Aquino <mario.e.aquino@gmail.com>
2011-03-06 12:00:45 -0600

    Changed settings defaults. Pages by default have top/middle/bottom sections.  Sections can also be added or removed.

--- db/seeds/refinery_settings.rb
+++ db/seeds/refinery_settings.rb
@@ -1,6 +1,7 @@
 [
   {:name => "site_name", :value => "Strange Loop"},
-  {:name => "new_page_parts", :value => false, :form_type => 'check_box'},
+  {:name => "new_page_parts", :value => true, :form_type => 'check_box'},
+  {:name => "default_page_parts", :value => ["Top", "Middle", "Bottom"]},
   {:name => "frontend_refinery_stylesheets_enabled", :value => false, :form_type => 'check_box'},
   {:name => "superuser_can_assign_roles", :value => true, :form_type => 'check_box'},
   {:name => "activity_show_limit", :value => 7},
