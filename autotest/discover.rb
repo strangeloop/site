8a21f2314cfe2adde12fea72bf85b6d61c861a4f
Mario Aquino <mario.e.aquino@gmail.com>
2011-04-08 07:12:53 -0500

    Updated discover logic to find any engines defined under vendor.

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,37 +1,37 @@
 Autotest.add_hook(:initialize) do |at|
   at.add_exception(%r{vendor/cache})
 
-  at.add_mapping(%r%^vendor/engines/proposals/spec/(models|controllers|routing|views|helpers|mailers|requests|lib)/.*rb$%) { |filename, _|
+  at.add_mapping(%r%^vendor/engines/\w+/spec/(models|controllers|routing|views|helpers|mailers|requests|lib)/.*rb$%) { |filename, _|
     filename
   }
-  at.add_mapping(%r%^vendor/engines/proposals/app/models/(.*)\.rb$%) { |_, m|
-    ["vendor/engines/proposals/spec/models/#{m[1]}_spec.rb"]
+  at.add_mapping(%r%^vendor/engines/(\w+)/app/models/(.*)\.rb$%) { |_, m|
+    ["vendor/engines/#{m[1]}/spec/models/#{m[2]}_spec.rb"]
   }
-  at.add_mapping(%r%^vendor/engines/proposals/app/views/(.*)$%) { |_, m|
-    at.files_matching %r%^vendor/engines/proposals/spec/views/#{m[1]}_spec.rb$%
+  at.add_mapping(%r%^vendor/engines/(\w+)/app/views/(.*)$%) { |_, m|
+    at.files_matching %r%^vendor/engines/#{m[1]}/spec/views/#{m[2]}_spec.rb$%
   }
-  at.add_mapping(%r%^vendor/engines/proposals/app/controllers/(.*)\.rb$%) { |_, m|
-    if m[1] == "application"
-      at.files_matching %r%^vendor/engines/proposals/spec/controllers/.*_spec\.rb$%
+  at.add_mapping(%r%^vendor/engines/(\w+)/app/controllers/(.*)\.rb$%) { |_, m|
+    if m[2] == "application"
+      at.files_matching %r%^vendor/engines/#{m[1]}/spec/controllers/.*_spec\.rb$%
     else
-      ["vendor/engines/proposals/spec/controllers/#{m[1]}_spec.rb"]
+      ["vendor/engines/#{m[1]}/spec/controllers/#{m[2]}_spec.rb"]
     end
   }
-  at.add_mapping(%r%^vendor/engines/proposals/app/helpers/(.*)_helper\.rb$%) { |_, m|
-    if m[1] == "application" then
-      at.files_matching(%r%^vendor/engines/proposals/spec/(views|helpers)/.*_spec\.rb$%)
+  at.add_mapping(%r%^vendor/engines/(\w+)/app/helpers/(.*)_helper\.rb$%) { |_, m|
+    if m[2] == "application" then
+      at.files_matching(%r%^vendor/engines/#{m[1]}/spec/(views|helpers)/.*_spec\.rb$%)
     else
-      ["vendor/engines/proposals/spec/helpers/#{m[1]}_helper_spec.rb"] + at.files_matching(%r%^spec\/views\/#{m[1]}/.*_spec\.rb$%)
+      ["vendor/engines/#{m[1]}/spec/helpers/#{m[2]}_helper_spec.rb"] + at.files_matching(%r%^spec\/views\/#{m[2]}/.*_spec\.rb$%)
     end
   }
-  at.add_mapping(%r%^vendor/engines/proposals/config/routes\.rb$%) {
-    at.files_matching %r%^vendor/engines/proposals/spec/(controllers|routing|views|helpers)/.*_spec\.rb$%
+  at.add_mapping(%r%^vendor/engines/(\w+)/config/routes\.rb$%) { |_, m|
+    at.files_matching %r%^vendor/engines/#{m[1]}/spec/(controllers|routing|views|helpers)/.*_spec\.rb$%
   }
-  at.add_mapping(%r%^vendor/engines/proposals/(spec/(spec_helper|support/.*)|config/(boot|environment(s/test)?))\.rb$%) {
-    at.files_matching %r%^vendor/engines/proposals/spec/(models|controllers|routing|views|helpers)/.*_spec\.rb$%
+  at.add_mapping(%r%^vendor/engines/(\w+)/(spec/(spec_helper|support/.*)|config/(boot|environment(s/test)?))\.rb$%) { |_, m|
+    at.files_matching %r%^vendor/engines/#{m[1]}/spec/(models|controllers|routing|views|helpers)/.*_spec\.rb$%
   }
-  at.add_mapping(%r%^vendor/engines/proposals/lib/(.*)\.rb$%) { |_, m|
-    ["vendor/engines/proposals/spec/lib/#{m[1]}_spec.rb"]
+  at.add_mapping(%r%^vendor/engines/(\w+)/lib/(.*)\.rb$%) { |_, m|
+    ["vendor/engines/#{m[1]}/spec/lib/#{m[2]}_spec.rb"]
   }
 end
 
