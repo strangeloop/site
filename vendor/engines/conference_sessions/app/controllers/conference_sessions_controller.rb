87b58ce02beea93348d64980913bba88f60dac65
Mario Aquino <mario.e.aquino@gmail.com>
2011-04-12 07:06:57 -0500

    Added workshops to public sessions page.

--- vendor/engines/conference_sessions/app/controllers/conference_sessions_controller.rb
+++ vendor/engines/conference_sessions/app/controllers/conference_sessions_controller.rb
@@ -3,6 +3,8 @@ class ConferenceSessionsController < ApplicationController
   before_filter :find_all_conference_sessions
 
   def index
+    @sessions_by_format = Hash.new{|hash, key| hash[key] = []}
+    @conference_sessions.each{|conf_session| @sessions_by_format[conf_session.format] << conf_session}
   end
 
   def show
--- vendor/engines/conference_sessions/spec/factories.rb
+++ vendor/engines/conference_sessions/spec/factories.rb
@@ -7,16 +7,33 @@ Factory.define :keynote_speaker, :parent => :speaker do |ks|
   ks.bio 'Single father, living a player lifestyle in California.'
 end
 
+Factory.define :workshop_speaker, :parent => :speaker do |ws|
+  ws.first_name 'Charlie'
+  ws.last_name 'Sheen'
+  ws.twitter_id 'adonisdna'
+  ws.company_url 'http://winning.com'
+end
+
 Factory.define :keynote_talk, :parent => :talk do |kt|
   kt.title 'God Hates Us All'
   kt.speakers { [Factory(:keynote_speaker)] }
 end
 
-Factory.define :conference_session do |cs|
-  cs.title 'God Hates Us All' #This title field in conference_session is a refinery wart
-  cs.talk { Factory(:keynote_talk) }
+Factory.define :workshop_talk, :parent => :talk do |wt|
+  wt.title 'Winning with Tiger Blood'
+  wt.speakers { [Factory(:workshop_speaker)] }
 end
 
-Factory.define :keynote_session, :parent => :conference_session do
+Factory.define :conference_session do |cs|
+end
+
+Factory.define :keynote_session, :parent => :conference_session do |ks|
+  ks.format 'keynote'
+  ks.talk { Factory(:keynote_talk) }
+end
+
+Factory.define :workshop_session, :parent => :conference_session do |ws|
+  ws.format 'workshop'
+  ws.talk { Factory(:workshop_talk) }
 end
 
