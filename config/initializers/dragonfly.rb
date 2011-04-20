259fca16e7796762b32f379bcb9f8ff55a591422
Mario Aquino <mario.e.aquino@gmail.com>
2011-04-20 05:54:38 -0500

    Minor cleanup of labels and commented-out code.

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,18 +1,9 @@
 require 'dragonfly'
 
-#app = Dragonfly[:strangeloop]
-#app.configure_with(:imagemagick)
-#app.configure_with(:rails)
-
-#app.define_macro(ActiveRecord::Base, :db_image_accessor)
-#app.datastore = RelationalDragonflyStore.new
-
-
 Rails.configuration.after_initialize do
   app = Dragonfly[:images]
   app.configure_with(:rails) do |c|
     c.datastore = RelationalDragonflyStore.new
-    #c.datastore.root_path = Rails.root.join('public', 'system', 'images').to_s
     c.url_path_prefix = '/system/images'
     c.secret = RefinerySetting.find_or_set(:dragonfly_secret,
                                           Array.new(24) { rand(256) }.pack('C*').unpack('H*').first)
