66319bcf1343c259c2aa7bd0aa12dfe4e7988ca5
Mario Aquino <mario.e.aquino@gmail.com>
2011-04-28 06:26:04 -0500

    Make speaker image editable after talk submission.

--- vendor/engines/conference_sessions/app/controllers/admin/conference_sessions_controller.rb
+++ vendor/engines/conference_sessions/app/controllers/admin/conference_sessions_controller.rb
@@ -10,5 +10,19 @@ module Admin
       render :partial => 'conference_sessions' if request.xhr?
     end
 
+    def update
+      conf = ConferenceSession.find(params[:id])
+      image_param = params[:conference_session][:talk_attributes][:speakers_attributes]["0"].delete(:image)
+      if image_param
+        image = Image.new(image_param)
+        image.save
+        params[:conference_session][:talk_attributes][:speakers_attributes]["0"][:image] = image
+      end
+      if conf.update_attributes! params[:conference_session]
+        flash[:notice] = 'Update successful'
+      end
+      redirect_to edit_admin_conference_session_path(conf)
+    end
+
   end
 end
