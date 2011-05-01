1e2b2a8147f532dd136b15ef4d42ce0510272c3b
Ryan Senior <senior.ryan@gmail.com>
2011-04-30 23:33:39 -0500

    Renamed the AddConfYear migration

--- db/migrate/20110122040052_add_conf_year.rb
+++
@@ -1,9 +0,0 @@
-class AddConfYear < ActiveRecord::Migration
-  def self.up
-    add_column :speakers, :conf_year, :integer
-  end
-
-  def self.down
-    remove_column :speakers, :conf_year
-  end
-end
