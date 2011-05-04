0911d8f6e32222b4e6cc72761504795460325cdf
Ryan Senior <senior.ryan@gmail.com>
2011-05-03 21:10:25 -0500

    Added an actionmailer configuration template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110430230834) do
+ActiveRecord::Schema.define(:version => 20110503040021) do
 
   create_table "comments", :force => true do |t|
     t.string   "title",            :limit => 50, :default => ""
@@ -82,12 +82,26 @@ ActiveRecord::Schema.define(:version => 20110430230834) do
     t.datetime "updated_at"
   end
 
+  create_table "news_item_translations", :force => true do |t|
+    t.integer  "news_item_id"
+    t.string   "locale"
+    t.string   "external_url"
+    t.text     "body"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "news_item_translations", ["news_item_id"], :name => "index_news_item_translations_on_news_item_id"
+
   create_table "news_items", :force => true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "publish_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "external_url"
+    t.integer  "image_id"
   end
 
   add_index "news_items", ["id"], :name => "index_news_items_on_id"
