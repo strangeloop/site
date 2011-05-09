2ff3a47d572b5496ffd57ee4922cb6084d2b5b2f
Mario Aquino <mario.e.aquino@gmail.com>
2011-05-09 07:47:44 -0500

    Hard coded db column size for image blobs

--- db/migrate/20110227213422_create_datastore_images_table.rb
+++ db/migrate/20110227213422_create_datastore_images_table.rb
@@ -2,7 +2,7 @@ class CreateDatastoreImagesTable < ActiveRecord::Migration
   def self.up
     create_table :datastore_images  do |t|
       t.string :uid
-      t.binary :image, :limit => 10.megabyte
+      t.binary :image, :limit => 16777215 #mysql size for medium blob
       t.timestamps
     end
 
