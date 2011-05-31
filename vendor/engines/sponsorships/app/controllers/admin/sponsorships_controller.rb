16086523556a979fc5893f28497ff46682fc0079
Mario Aquino <mario.e.aquino@gmail.com>
2011-05-30 21:55:19 -0500

    Fixes #18. This commit resolves the issue with the sponsorship level not displaying the correct value from previous save in the admin/edit form.

--- vendor/engines/sponsorships/app/controllers/admin/sponsorships_controller.rb
+++ vendor/engines/sponsorships/app/controllers/admin/sponsorships_controller.rb
@@ -3,7 +3,6 @@ module Admin
     include ImageUploadFix
 
     prepend_before_filter :find_all_levels, :only => [:new, :edit]
-    prepend_before_filter :resolve_sponsorship_level, :only => [:create, :update]
 
     crudify :sponsorship, :xhr_paging => true
 
@@ -18,11 +17,6 @@ module Admin
 
     def find_all_levels
       @levels = SponsorshipLevel.all
-    end
-
-    def resolve_sponsorship_level
-      level = SponsorshipLevel.find(params['sponsorship']['sponsorship_level'].to_i)
-      params['sponsorship']['sponsorship_level'] = level
     end
   end
 end
