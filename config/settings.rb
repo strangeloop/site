59ba1fc3611327160474ab8c08bda1853773997b
Mario Aquino <mario.e.aquino@gmail.com>
2011-06-03 09:00:57 -0500

    Switched to Apache 2 license.  Added rake task to adds copyright notice to top of all ruby files.  Fixes #47

--- config/settings.rb
+++ config/settings.rb
@@ -1,3 +1,20 @@
+#- Copyright 2011 Strange Loop LLC
+#- 
+#- Licensed under the Apache License, Version 2.0 (the "License");
+#- you may not use this file except in compliance with the License.
+#- You may obtain a copy of the License at
+#- 
+#-    http://www.apache.org/licenses/LICENSE-2.0
+#- 
+#- Unless required by applicable law or agreed to in writing, software
+#- distributed under the License is distributed on an "AS IS" BASIS,
+#- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#- See the License for the specific language governing permissions and 
+#- limitations under the License.
+#- 
+
+
+
 # Other settings go here as below.
 # These settings are ones that are set for all environments and need to be set AFTER the refinery boot process.
 Refinery.authentication_login_field = 'login'
