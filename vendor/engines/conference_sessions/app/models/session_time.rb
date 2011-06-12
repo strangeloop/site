6a1f4f75712799b002735b48fb53ad40b751ef35
Mario Aquino <mario.e.aquino@gmail.com>
2011-06-11 22:43:51 -0500

    Added field validations for SessionTime model.

--- vendor/engines/conference_sessions/app/models/session_time.rb
+++ vendor/engines/conference_sessions/app/models/session_time.rb
@@ -1,4 +1,20 @@
 class SessionTime < ActiveRecord::Base
+  validates_presence_of :start_time
+
+  validates_numericality_of :duration_hours, :greater_than => 0,
+    :unless => Proc.new {|st| st.duration_minutes > 0 }
+
+  validates_numericality_of :duration_minutes, :greater_than => 0,
+    :unless => Proc.new {|st| st.duration_hours > 0 }
+
+  def duration_hours
+    self[:duration_hours] || 0
+  end
+
+  def duration_minutes
+    self[:duration_minutes] || 0
+  end
+
   def title
     return "" unless start_time && duration_hours && duration_minutes
     end_time = start_time.advance(:hours => duration_hours, :minutes => duration_minutes)
--- vendor/engines/conference_sessions/spec/models/session_time_spec.rb
+++ vendor/engines/conference_sessions/spec/models/session_time_spec.rb
@@ -8,4 +8,24 @@ describe SessionTime do
   its "title contains start and end time" do
     Factory(:session_time).title.should eq('Tuesday from 12:30 to 01:30 PM')
   end
+
+  it "is invalid without start_time" do
+    SessionTime.new.should_not be_valid
+  end
+
+  it "is invalid if both duration_hours and duration_minutes are zero" do
+    SessionTime.new(:start_time => DateTime.now).should_not be_valid
+  end
+
+  it "is valid if start_time and duration_hours are defined" do
+    SessionTime.new(:start_time => DateTime.now, :duration_hours => 1).should be_valid
+  end
+
+  it "is valid if start_time and duration_minutes are defined" do
+    SessionTime.new(:start_time => DateTime.now, :duration_minutes => 1).should be_valid
+  end
+
+  it "is valid if start_time, duration_hours, and duration_minutes are non-zero" do
+    SessionTime.new(:start_time => DateTime.now, :duration_hours => 1, :duration_minutes => 1).should be_valid
+  end
 end
