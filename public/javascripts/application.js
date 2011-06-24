fe4ac823b6bcf9cd76fed55ed0fb50935e501a10
Mario Aquino <mario.e.aquino@gmail.com>
2011-06-24 08:15:46 -0500

    Added multi-track toggle for the schedule page.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
   $('.trackball').toggle(function() {
     var track=$(this).parent().attr("data-track");
-    $('.column2:not([data-track="' + track + '"])').addClass("shaded");
+    if ($(this).parent().hasClass("shaded")) {
+      removeShade(similarTracks(track));
+    } else {
+      addShade(differentTracks(track));
+    }
   },
   function() {
     var track=$(this).parent().attr("data-track");
-    $('.column2:not([data-track="' + track + '"])').removeClass("shaded");
+    if ($(this).parent().hasClass("shaded")) {
+      addShade(similarTracks(track));
+    } else {
+      removeShade(differentTracks(track));
+    }
   });
+  function similarTracks(track) {
+    return $('.column2:[data-track="' + track + '"]');
+  };
+  function differentTracks(track) {
+    return $('.column2:not([data-track="' + track + '"])');
+  };
+  function removeShade(tracks) {
+    tracks.removeClass("shaded");
+  };
+  function addShade(tracks) {
+    tracks.addClass("shaded");
+  };
 });
 
