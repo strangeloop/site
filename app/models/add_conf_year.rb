6532796db1d04af832e4dc8736eea1d738a806ab
Mario Aquino <mario.e.aquino@gmail.com>
2011-07-09 12:31:25 -0500

    Empty whitespace cleanup

--- app/models/add_conf_year.rb
+++ app/models/add_conf_year.rb
@@ -1,17 +1,17 @@
 #- Copyright 2011 Strange Loop LLC
-#- 
+#-
 #- Licensed under the Apache License, Version 2.0 (the "License");
 #- you may not use this file except in compliance with the License.
 #- You may obtain a copy of the License at
-#- 
+#-
 #-    http://www.apache.org/licenses/LICENSE-2.0
-#- 
+#-
 #- Unless required by applicable law or agreed to in writing, software
 #- distributed under the License is distributed on an "AS IS" BASIS,
 #- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#- See the License for the specific language governing permissions and 
+#- See the License for the specific language governing permissions and
 #- limitations under the License.
-#- 
+#-
 
 
 
@@ -22,4 +22,4 @@ class AddConfYear
   def self.before_create(model)
     model.conf_year= Time.now.year unless model.conf_year
   end
-end  
+end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,17 +1,17 @@
 #- Copyright 2011 Strange Loop LLC
-#- 
+#-
 #- Licensed under the Apache License, Version 2.0 (the "License");
 #- you may not use this file except in compliance with the License.
 #- You may obtain a copy of the License at
-#- 
+#-
 #-    http://www.apache.org/licenses/LICENSE-2.0
-#- 
+#-
 #- Unless required by applicable law or agreed to in writing, software
 #- distributed under the License is distributed on an "AS IS" BASIS,
 #- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#- See the License for the specific language governing permissions and 
+#- See the License for the specific language governing permissions and
 #- limitations under the License.
-#- 
+#-
 
 
 
@@ -27,9 +27,9 @@ class Speaker < ActiveRecord::Base
   belongs_to :image
 
   before_create AddConfYear
-  
+
   def to_s
-  	first_name + " " + last_name
+    first_name + " " + last_name
   end
 
   def twitter_id= id
