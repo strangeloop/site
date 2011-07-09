4cec68aec46eb71fbfbc19924b78ffc770552da3
Mario Aquino <mario.e.aquino@gmail.com>
2011-07-09 12:31:25 -0500

    Empty whitespace cleanup

--- app/models/customized_page.rb
+++ app/models/customized_page.rb
@@ -1,24 +1,24 @@
 #- Copyright 2011 Strange Loop LLC
-#- 
+#-
 #- Licensed under the Apache License, Version 2.0 (the "License");
 #- you may not use this file except in compliance with the License.
 #- You may obtain a copy of the License at
-#- 
+#-
 #-    http://www.apache.org/licenses/LICENSE-2.0
-#- 
+#-
 #- Unless required by applicable law or agreed to in writing, software
 #- distributed under the License is distributed on an "AS IS" BASIS,
 #- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#- See the License for the specific language governing permissions and 
+#- See the License for the specific language governing permissions and
 #- limitations under the License.
-#- 
+#-
 
 
 
-#This customization of the Page class adds cache clearing after the 
-#expire_page_cache callback, which is invoked on page adds and 
+#This customization of the Page class adds cache clearing after the
+#expire_page_cache callback, which is invoked on page adds and
 #deletes.  This customization clears the header & footer caches
-#whenever pages are added or removed so that they navigation 
+#whenever pages are added or removed so that they navigation
 #bar links at the top and bottom of all pages can be regenerated.
 class PageCustomizer
   def self.load
