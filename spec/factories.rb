9896c71fa84247d6fd09c88a8c6c00b29ee74255
Mario Aquino <mario.e.aquino@gmail.com>
2011-07-12 07:12:48 -0500

    Added attendee record.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,17 @@
 #- Copyright 2011 Strange Loop LLC
-#- 
+#-
 #- Licensed under the Apache License, Version 2.0 (the "License");
 #- you may not use this file except in compliance with the License.
 #- You may obtain a copy of the License at
-#- 
+#-
 #-    http://www.apache.org/licenses/LICENSE-2.0
-#- 
+#-
 #- Unless required by applicable law or agreed to in writing, software
 #- distributed under the License is distributed on an "AS IS" BASIS,
 #- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#- See the License for the specific language governing permissions and 
+#- See the License for the specific language governing permissions and
 #- limitations under the License.
-#- 
+#-
 
 
 
@@ -265,11 +265,18 @@ Factory.define :attendee_user, :parent => :user do |u|
   u.roles { [ Role[:refinery] ] }
 end
 
-Factory.define :attendee do |um|
-  um.first_name "Julian"
-  um.last_name "English"
-  um.email "julian_english@prodigy.net"
-  um.reg_id "1234"
+Factory.define :attendee do |a|
+  a.user { Factory(:user_for_attendee) }
+  a.first_name 'Kaiser'
+  a.middle_name 'Von'
+  a.last_name 'Sozhay'
+  a.city 'St. Louis'
+  a.state 'MO'
+  a.country 'United States of America'
+  a.email 'kaiser@meatspace.com'
+  a.twitter_id 'kaiser'
+  a.blog_url 'http://kaiser.blogspot.com'
+  a.reg_id 'abc'
+  a.reg_status 'paid'
   um.token_created_at DateTime.parse('Thursday, July 4, 2011 11:19 AM')
 end
-
