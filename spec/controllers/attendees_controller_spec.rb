976c1e64a4e0f1f00caf12ba80b528dd2640795f
Mario Aquino <mario.e.aquino@gmail.com>
2011-07-27 08:30:17 -0500

    Added pagination on the controller side for the attendees index.

--- spec/controllers/attendees_controller_spec.rb
+++ spec/controllers/attendees_controller_spec.rb
@@ -16,7 +16,19 @@ describe AttendeesController do
   it "loads all attendees from current year for index action" do
     attendee
     get :index
-    controller.current_year_attendees.should == [attendee]
+    controller.current_year_attendees.should eq([attendee])
+  end
+
+  it "paginates on #index" do
+    attendee
+    get :index, :page => 1
+    controller.current_year_attendees.should eq([attendee])
+  end
+
+  it "returns an empty list on a page request past the total on #index" do
+    attendee
+    get :index, :page => 2
+    controller.current_year_attendees.should be_empty
   end
 
   context "#edit" do
@@ -24,7 +36,7 @@ describe AttendeesController do
       @request.env['devise.mapping'] = :admin
       sign_in attendee.user
     end
-    
+
     it "exposes a specific attendee for #edit" do
       get :edit
       controller.attendee.should eq(attendee)
