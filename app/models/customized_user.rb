a44c07c969eeb22b0e4a817a264edce3f50e90f0
Ryan Senior <senior.ryan@gmail.com>
2011-08-08 00:36:46 -0500

    Added a new method for debugging to user

--- app/models/customized_user.rb
+++ app/models/customized_user.rb
@@ -25,6 +25,9 @@ class UserCustomizer
 
   def self.load
     User.class_eval do
+      def foo
+        "foo"
+      end
       has_many :services, :dependent => :destroy
       debugger
       ajaxful_rater
