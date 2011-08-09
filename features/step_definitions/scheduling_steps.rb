6ba5e97015c22531b9379e313b6c819ecfac599d
Mario Aquino <mario.e.aquino@gmail.com>
2011-08-09 18:27:24 -0500

    Show message when no schedule has been filled out.

--- features/step_definitions/scheduling_steps.rb
+++ features/step_definitions/scheduling_steps.rb
@@ -1,17 +1,17 @@
 #- Copyright 2011 Strange Loop LLC
-#- 
+#-
 #- Licensed under the Apache License, Version 2.0 (the "License");
 #- you may not use this file except in compliance with the License.
 #- You may obtain a copy of the License at
-#- 
+#-
 #-    http://www.apache.org/licenses/LICENSE-2.0
-#- 
+#-
 #- Unless required by applicable law or agreed to in writing, software
 #- distributed under the License is distributed on an "AS IS" BASIS,
 #- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#- See the License for the specific language governing permissions and 
+#- See the License for the specific language governing permissions and
 #- limitations under the License.
-#- 
+#-
 
 
 Given /^there are no rooms$/ do
@@ -25,4 +25,9 @@ end
 Given /^there are no tracks$/ do
   Track.destroy_all
 end
+
+Then /^I should see "([^"]*)" as a link to the schedule page$/ do |link_text|
+  page.has_link?(link_text, :href => schedule_path).should be_true
+end
+
 
