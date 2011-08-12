ba820f31bd919ab34f04e012c72c5f45466cfb62
Mario Aquino <mario.e.aquino@gmail.com>
2011-08-12 16:18:00 -0500

    Minor cleanup

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -56,7 +56,6 @@ class Attendee < ActiveRecord::Base
       conference_sessions << ConferenceSession.find(session_id)
       true
     rescue ActiveRecord::RecordNotFound
-      nil
     end
   end
 
