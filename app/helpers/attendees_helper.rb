1c645709d5470ee4975a911b76f06e065fa38e44
Mario Aquino <mario.e.aquino@gmail.com>
2011-08-13 13:38:00 -0500

    Only show "Update my profile" link if you are looking at your own page. Also, conditionally show the profile fields when they are filled out for the attendee.

--- app/helpers/attendees_helper.rb
+++ app/helpers/attendees_helper.rb
@@ -6,4 +6,8 @@ module AttendeesHelper
   def schedule_partial(attendee)
     attendee.conference_sessions.empty? ? 'no_schedule' : 'populated_schedule'
   end
+
+  def is_current_user?(attendee)
+    current_user == attendee.user
+  end
 end
