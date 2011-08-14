b83463c81abe265f329a55b8547a9258eb5259ba
Ryan Senior <senior.ryan@gmail.com>
2011-08-14 10:47:26 -0500

    removed debugger line

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -2,7 +2,7 @@ class AccountActivationController < ApplicationController
   before_filter :authenticate_user!, :except => [:new, :show]
   def new
     attendee = Attendee.check_token(params[:token]) if params[:token]
-    debugger
+
     if attendee
       @attendee = attendee
     else
