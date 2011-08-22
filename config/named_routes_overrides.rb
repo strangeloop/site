45f6893993df7938bcc4ebfb35f5c80562647e58
Ryan Senior <senior.ryan@gmail.com>
2011-08-21 21:24:07 -0500

    small named routes change

--- config/named_routes_overrides.rb
+++ config/named_routes_overrides.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users,
-   :only => :sign_in,
+
   :path_names => {:sign_in => 'login', :sign_up => 'activation'},
-   :path => '',
-   :controllers => {:sign_in => 'attendee_login/create', :sign_up => 'account_activation/create'} do
+  :path => '',
+  :controllers => {:sign_up => 'account_activation/create'} do
 
   #any additional user session routes would be defined here
   end
 
-  get "static/home", :as=>"user_root"
+  get "attendees/index", :as=>"root"
 end
 
