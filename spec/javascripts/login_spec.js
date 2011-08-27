e4881a732a8bc84fd818d5f7099d92a24a52cf93
Mario Aquino <mario.e.aquino@gmail.com>
2011-08-27 07:36:27 -0500

    Fixed case where prepended/appended nav bar links would go invisible on hover because they weren't wrapped in span tags.

--- spec/javascripts/login_spec.js
+++ spec/javascripts/login_spec.js
@@ -2,16 +2,16 @@ describe("Nav bar re-writing", function() {
   beforeEach(function() {
     loadFixtures('nav_bar.html');
   });
-  
+
   it("adds name link to the beginning of the nav list", function() {
     SL.prependNav('foo', '/bar');
-    expect($('nav > ul li:first-child').html()).toEqual('<a href="/bar">foo</a>');
+    expect($('nav > ul li:first-child').html()).toEqual('<a href="/bar"><span>foo</span></a>');
   });
 
   it("adds log-out to the end of the nav list", function() {
     SL.appendNav('Log Out', '/logout');
-    expect($('nav > ul li:last').html()).toEqual('<a href="/logout">Log Out</a>');
-  });  
-  
-});  
+    expect($('nav > ul li:last').html()).toEqual('<a href="/logout"><span>Log Out</span></a>');
+  });
+
+});
 
