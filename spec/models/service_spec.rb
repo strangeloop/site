410833ebd5ef4aee43214d32932685b03d3db404
Ryan Senior <senior.ryan@gmail.com>
2011-08-30 21:55:07 -0500

    added test for the google service factory method

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -8,4 +8,19 @@ describe Service do
   it {should belong_to :attendee_cred}
 
   it {should have_db_column(:uemail).of_type(:string)}
+
+  let(:google_omniauth) {{'uid' => 'Henry',
+    'provider' => 'google',
+    'user_info' =>
+      {'email' => 'henry@chinaski.com',
+        'name' => 'Henry Chinaski'}}}
+
+  it "should create a google service" do
+    svc = Service.google_service google_omniauth
+    svc.uemail.should == 'henry@chinaski.com'
+    svc.uname.should == 'Henry Chinaski'
+    svc.uid.should == 'Henry'
+    svc.provider.should == 'google'
+  end
+  
 end
