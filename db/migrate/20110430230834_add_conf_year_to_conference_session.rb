dea37bca906b8921afe82cc468a35fa57043d30d
Mario Aquino <mario.e.aquino@gmail.com>
2012-01-08 15:02:12 -0600

    Remove 2011 year defaults for model conf_year attribute.

--- db/migrate/20110430230834_add_conf_year_to_conference_session.rb
+++ db/migrate/20110430230834_add_conf_year_to_conference_session.rb
@@ -1,6 +1,6 @@
 class AddConfYearToConferenceSession < ActiveRecord::Migration
   def self.up
-    add_column :conference_sessions, :conf_year, :integer, :default => Time.now.year
+    add_column :conference_sessions, :conf_year, :integer
     remove_column :talks, :conf_year
   end
 
