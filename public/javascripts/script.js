531d6ecf01f87b243f0493d53182cdab41fd8113
Mario Aquino <mario.e.aquino@gmail.com>
2012-01-28 12:41:05 -0600

    Remove commented-out and unused code.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -55,21 +55,6 @@ $(document).ready(function() {
 				   });
 			};
 
-	/*
-			$('.sidebar_menu > li.parrent').toggle(
-			function() {
-				$(this).addClass('sidebar_menu_act')
-				$(this).find('> .sidebar_menu').stop(false, true).slideDown(500);
-			},
-			function() {
-				$(this).removeClass('sidebar_menu_act')
-				$(this).find('> .sidebar_menu').stop(false, true).slideUp(500);
-				Cufon.refresh();
-			}
-		);
-		*/
-
-
 				$("#pricing_table .grid_price").hover(function() {
 					$(this).stop().animate({ marginTop: "-10px", paddingTop:"25px", marginBottom: "-10px",  marginLeft: "-5px",  marginRight: "-5px", paddingLeft: "25px", paddingRight: "25px",  width: "180px",  height: "358px"  }, 200);
 					$(this).find('.bottom_bg').stop().animate({height:"69px"}, 200);
@@ -91,33 +76,6 @@ $(document).ready(function() {
 					$(this).find('.bottom_bg div').stop().animate({height:"59px"},300);
 				});
 
-		/*	if (jQuery(".kwicks").length) {
-			  jQuery('.kwicks .slide .kwicks_title').css({ bottom: "0" });
-			  jQuery('.kwicks .slide').fadeIn(300);
-
-			  jQuery('.kwicks').kwicks({
-			   max :  940,
-			   spacing : 0
-			  });
-
-
-					jQuery('.kwicks .slide').hover(function() {
-					jQuery('.kwicks_title', this).hide();
-			  jQuery('.kwicks_description', this).stop().fadeTo(900, 0.8);
-				   jQuery('.kwicks_title', this).css({ top: "0" });
-					jQuery('.kwicks_title', this).css({ bottom: "" });
-					jQuery('.kwicks_title', this).stop().fadeTo(900, 0.8);
-			  },function(){
-			  jQuery('.kwicks_description', this).hide();
-				   jQuery('.kwicks_title', this).hide();
-					jQuery('.kwicks_title', this).css({ top: "" });
-					jQuery('.kwicks_title', this).css({ bottom: "0" });
-					jQuery('.kwicks_title', this).stop().fadeTo(900, 0.8);
-
-			 });
-				}
-			*/
-
 if ($(".checked_block").length) {
 $('input:checkbox:not([safari])').checkbox();
 }
@@ -128,92 +86,6 @@ $('input:checkbox:not([safari])').checkbox();
                 return value != element.defaultValue;
         }, "");
 
-
-			jQuery("#contactform").validate({
-
-			submitHandler: function(form) {
-
-				var $ = jQuery;
-
-				$('#loader').fadeIn();
-
-				var formData = $(form).serialize(),
-					note = $('#Note');
-
-				$.ajax({
-					type: "POST",
-					url: "send.php",
-					data: formData,
-					success: function(response) {
-						if ( note.height() ) {
-							note.fadeIn('fast', function() { $(this).hide(); });
-						} else {
-							note.hide();
-						}
-
-						$('#LoadingGraphic').fadeOut('fast', function() {
-							//$(this).remove();
-							if (response === 'success') {
-								$('#contactform').animate({opacity: 0},'fast');
-							}
-
-							// Message Sent? Show the 'Thank You' message and hide the form
-							result = '';
-							c = '';
-							if (response === 'success') {
-								result = 'Your message has been sent. Thank you!';
-								c = 'success';
-							}
-
-							note.removeClass('success').removeClass('error').text('');
-							var i = setInterval(function() {
-								if ( !note.is(':visible') ) {
-									note.html(result).addClass(c).slideDown('fast');
-									clearInterval(i);
-								}
-							}, 40);
-						});
-					}
-				});
-
-				return false;
-			},
-
-			 rules:
-      {
-       formname: "required defaultInvalid",
-	   formcomments: "required defaultInvalid"
-      },
-
-			 messages: {
-    		 formname: "Please specify your name.",
-			 formcomments: "Please enter your message.",
-    		 formemail: {
-      			 required: "We need your email address to contact you.",
-      			 email: "Your email address must be in the format of name@domain.com"
-    		 }
-  		 }
-		});
-
-
-/*
-			//When page loads...
-			$(".tab_content").hide(); //Hide all content
-			$("ul.tabs li:first").addClass("active").show(); //Activate first tab
-			$(".tab_content:first").show(); //Show first tab content
-
-			//On Click Event
-			$("ul.tabs li").click(function() {
-
-				$("ul.tabs li").removeClass("active"); //Remove any "active" class
-				$(this).addClass("active"); //Add "active" class to selected tab
-				$(".tab_content").hide(); //Hide all tab content
-
-				var activeTab = $(this).find("a").attr("href"); //Find the title attribute value to identify the active tab + content
-				$(activeTab).fadeIn(); //Fade in the active ID content
-				return false;
-			});
- $('input:checkbox:not([safari])').checkbox(); */
 
 });
 jQuery('.list1 li').hover(function(){
