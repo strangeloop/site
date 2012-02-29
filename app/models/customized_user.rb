c51b5259b1737348c486971563a000d62e55f531
Ryan Senior <senior.ryan@gmail.com>
2012-02-29 00:18:37 -0600

    Code to call to regonline for authentication and parse the response

--- app/models/customized_user.rb
+++ app/models/customized_user.rb
@@ -27,6 +27,24 @@ class UserCustomizer
     User.class_eval do
       has_many :services, :dependent => :destroy
       ajaxful_rater
+
+      def self.successful_response?(xml)
+        xml_doc = Nokogiri::Slop(xml)
+        xml_doc.remove_namespaces!
+        xml_doc.ResultsOfListOfRegistration.Success.content == "true"
+      end
+
+      def self.authenticate_user(email, password, eventId)
+        url = URI.parse "https://www.regonline.com/webservices/default.asmx/LoginRegistrant"
+        req = Net::HTTP::Post.new(url.path)
+        req.set_form_data({'email' => email,
+                            'password' => password,
+                            'eventID' => eventId})
+        https = Net::HTTP.new(url.host, url.port)
+        https.use_ssl = true
+        resp = https.start { |cx| cx.request(req) }
+        successful_response resp
+      end
     end
   end
 end
