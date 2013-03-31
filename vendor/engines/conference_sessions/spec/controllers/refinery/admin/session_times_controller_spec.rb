41728d2aa612aaeb8fec3e084cd0ec09d5111ed8
Mario Aquino <mario.e.aquino@gmail.com>
2013-03-31 13:08:00 -0500

    Update load path

--- vendor/engines/conference_sessions/spec/controllers/refinery/admin/session_times_controller_spec.rb
+++ vendor/engines/conference_sessions/spec/controllers/refinery/admin/session_times_controller_spec.rb
@@ -4,7 +4,7 @@ require_relative '../../../../app/controllers/refinery/admin/session_times_contr
 describe Refinery::Admin::SessionTimesController do
   it "sorts by start time ascending" do
     described_class.should_receive(:crudify).with(:session_time, {:order => 'start_time ASC'})
-    load(File.join(File.dirname(__FILE__),'..','..','..','app', 'controllers', 'refinery', 'admin', 'session_times_controller.rb'))
+    load(File.join(File.dirname(__FILE__),'..','..','..','..','app', 'controllers', 'refinery', 'admin', 'session_times_controller.rb'))
   end
 
 end
