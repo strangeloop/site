6b162244ef9867796a474afc1ebe9e2e7f49a434
Mario Aquino <mario.e.aquino@gmail.com>
2013-09-12 16:44:56 -0500

    Add free workshop as a session category

--- lib/session_formats.rb
+++ lib/session_formats.rb
@@ -1,5 +1,5 @@
 module SessionFormats
   def self.included(base)
-    base.instance_eval 'def self.format_options; %w(keynote workshop elc talk lightning undefined unsession strange\ passions panel miscellaneous); end'
+    base.instance_eval 'def self.format_options; %w(keynote workshop free\ workshop elc talk lightning undefined unsession strange\ passions panel miscellaneous); end'
   end
 end
--- vendor/engines/conference_sessions/app/controllers/conference_sessions_controller.rb
+++ vendor/engines/conference_sessions/app/controllers/conference_sessions_controller.rb
@@ -41,7 +41,7 @@ class ConferenceSessionsController < ApplicationController
   expose(:speaker) { talk.speakers.first }
 
   expose(:elc_schedule) { ConferenceSession::by_short_session_time_and_location_for_formats('elc') }
-  expose(:workshop_schedule) { ConferenceSession::by_session_time_for_formats('workshop') }
+  expose(:workshop_schedule) { ConferenceSession::by_session_time_for_formats('workshop', 'free workshop') }
   expose(:main_schedule) { ConferenceSession::by_short_session_time_and_location_for_formats('keynote', 'talk', 'miscellaneous', 'unsession') }
 
   def index
