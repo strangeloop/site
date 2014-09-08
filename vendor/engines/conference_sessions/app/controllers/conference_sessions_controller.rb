51624c9716e9979f2c9b67c8dea6fbd50a7bbc60
Mario Aquino <mario@outpace.com>
2014-09-07 21:56:40 -0500

    Using the wrong method for doing FPW session lookups.

--- vendor/engines/conference_sessions/app/controllers/conference_sessions_controller.rb
+++ vendor/engines/conference_sessions/app/controllers/conference_sessions_controller.rb
@@ -40,7 +40,7 @@ class ConferenceSessionsController < ApplicationController
   expose(:talk) { conference_session.talk }
   expose(:speaker) { talk.speakers.first }
 
-  expose(:fpw_schedule) { ConferenceSession::by_short_session_time_and_location_for_formats('fpw') }
+  expose(:fpw_schedule) { ConferenceSession::by_session_time_for_formats('fpw') }
   expose(:workshop_schedule) { ConferenceSession::by_session_time_for_formats('workshop', 'free workshop') }
   expose(:main_schedule) { ConferenceSession::by_short_session_time_and_location_for_formats('keynote', 'talk', 'miscellaneous', 'unsession') }
 
